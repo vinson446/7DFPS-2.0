4b5562e607c9c5500f097b21dfaa3c2033f60c4f
Benny <liangchengheng1@gmail.com>
2020-12-09 22:10:07 -0600

    bullet travel vfx

---
+++ Assets/Scripts/ParticleTestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTestScript : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    //[SerializeField] ParticleSystem 
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            muzzleFlash.Play();
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            muzzleFlash.Stop();
+        }
+    }
+}
